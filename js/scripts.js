97530db6f18a1805e96b13792d9b38e456bcb351
Ale <69171801+Ale-LS@users.noreply.github.com>
2023-05-07 19:22:45 -0400

    actualización -valeria

--- js/scripts.js
+++ js/scripts.js
@@ -44,22 +44,22 @@ $(document).ready(function () {
 
     //Validación para RUT chileno
     $.validator.addMethod("rutChileno", function (value, element) {
-        // Eliminar puntos y guión del RUT
+        
         value = value.replace(/[.-]/g, "");
 
-        // Validar que el RUT tenga 8 o 9 dígitos
+        
         if (value.length < 8 || value.length > 9) {
             return false;
         }
 
-        // Validar que el último dígito sea un número o una 'K'
+        
         var validChars = "0123456789K";
         var lastChar = value.charAt(value.length - 1).toUpperCase();
         if (validChars.indexOf(lastChar) == -1) {
             return false;
         }
 
-        // Calcular el dígito verificador
+        
         var rut = parseInt(value.slice(0, -1), 10);
         var factor = 2;
         var sum = 0;
@@ -73,7 +73,7 @@ $(document).ready(function () {
         var dv = 11 - (sum % 11);
         dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
 
-        // Validar que el dígito verificador sea correcto
+        
         return dv === lastChar;
     }, "Por favor ingrese un RUT válido.");
 
