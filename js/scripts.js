5bd6e2dd4a12543b0ab90b967d83d63ad5a44081
jeralxs <129078128+jeralxs@users.noreply.github.com>
2023-05-24 12:47:30 -0400

    Update scripts.js
    validador mantenedor usuarios

--- js/scripts.js
+++ js/scripts.js
@@ -34,10 +34,10 @@ fetch('https://fakestoreapi.com/products')
         card.appendChild(button);
         cardsContainer.appendChild(card);
         });
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos de la API', error);
     });
-})
-.catch(error => {
-    console.error('Error al obtener los datos de la API', error);
 });
 
 $(document).ready(function () {
@@ -275,3 +275,113 @@ $(document).ready(function(){
         },
     });
 });
+
+//Validador formulario mantenedor usuarios
+$(document).ready(function () {
+
+    //Validación para RUT chileno
+    $.validator.addMethod("rutChileno", function (value, element) {
+        
+        value = value.replace(/[.-]/g, "");
+
+        
+        if (value.length < 8 || value.length > 9) {
+            return false;
+        }
+
+        
+        var validChars = "0123456789K";
+        var lastChar = value.charAt(value.length - 1).toUpperCase();
+        if (validChars.indexOf(lastChar) == -1) {
+            return false;
+        }
+
+        
+        var rut = parseInt(value.slice(0, -1), 10);
+        var factor = 2;
+        var sum = 0;
+        var digit;
+        while (rut > 0) {
+            digit = rut % 10;
+            sum += digit * factor;
+            rut = Math.floor(rut / 10);
+            factor = factor === 7 ? 2 : factor + 1;
+        }
+        var dv = 11 - (sum % 11);
+        dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+        
+        return dv === lastChar;
+    }, "Por favor ingrese un RUT válido.");
+
+    $("#formusuarios").validate({
+        rules: {
+            id: {
+                required: true,
+            },
+            tipocliente: {
+                required: true,
+                maxlength: 2
+            },
+            rut: {
+                required: true,
+                rutChileno: true,
+            },
+            nombres: {
+                required: true,
+            },
+            apellidos: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            direccion: {
+                required: true,
+            },
+            contrasena: {
+                required: true,
+                minlength: 8,
+            },
+        },
+        messages: {
+            id: {
+                required: "El ID es un campo obligatorio"
+            },
+            tipocliente: {
+                required: "Debes elegir una opción",
+                maxlength: "Elige solo {1} opción"
+            },
+            rut: {
+                required: "El RUT es un campo obligatorio",
+                rutChileno: "El formato del rut no es válido",
+            },
+            nombres: {
+                required: "El nombre es un campo obligatorio",
+            },
+            apellidos: {
+                required: "El apellido es un campo obligatorio",
+            },
+            email: {
+                required: "El correo es un campo obligatorio",
+                email: "El correo no cumple con el formato",
+            },
+            direccion: {
+                required: "La dirección es un campo obligatorio"
+            },
+            contrasena: {
+                required: "La contraseña es una campo obligatorio",
+                minlength: "Mínimo 8 caracteres",
+            },
+        },
+        errorPlacement: function(error,element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+});    
