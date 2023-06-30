848c4fa41354ac8920f8d7050f86fc1fc835f9b8
Lía Da Silva Rojas <lia.dasilva.info@gmail.com>
2023-06-29 21:37:03 -0400

    todo lo de productos!

--- GrupoBkn/core/urls.py
+++ GrupoBkn/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, poblar_bd, producto, producto_tienda, producto_ficha
+from .views import home, producto, producto_tienda, producto_ficha
 
 urlpatterns = [
     path('',home,name="home"),
-    path('poblar_bd', poblar_bd, name="poblar_bd"),
     path('producto/<action>/<id>', producto, name="producto"),
     path('producto_tienda', producto_tienda, name="producto_tienda"),
     path('producto_ficha/<id>', producto_ficha, name="producto_ficha"),
